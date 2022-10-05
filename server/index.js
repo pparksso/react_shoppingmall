31cdf4cc18ea4aecab249c09f7a458c05b5652df
pparksso <thgml5143@hanmail.net>
2022-10-05 22:45:18 +0900

    index.js 수정

--- server/index.js
+++ server/index.js
@@ -31,9 +31,9 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "/build")));
 app.set("port", process.env.PORT || 8099);
-app.get;
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/build/index.html");
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 app.use("/user", userRouter);
 app.use("/cart", cartRouter);
